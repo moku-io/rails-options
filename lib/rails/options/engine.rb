410c66f602a4dd775e16731512241dd1fd3aaca5
Riccardo Agatea <riccardo@moku.io>
2023-09-22 11:34:10 +0200

    add environment specific options.yml

--- lib/rails/options/engine.rb
+++ lib/rails/options/engine.rb
@@ -4,7 +4,7 @@ module Rails
       config.options = ActiveSupport::OrderedOptions.new
 
       config.options.roots = ['config']
-      config.options.paths = ['options.{yml,yaml}{.enc,}', 'options/**/*.{yml,yaml}{.enc,}']
+      config.options.paths = ['options{,.*}.{yml,yaml}{.enc,}', 'options/**/*.{yml,yaml}{.enc,}']
       config.options.raise_on_override = false
     end
   end
