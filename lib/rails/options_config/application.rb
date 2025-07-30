ffdc85dc959d80df8a3cfd922ac57ac3dc048749
Riccardo Agatea <riccardo@moku.io>
2025-07-30 15:50:15 +0200

    allow caching in more cases

--- lib/rails/options_config/application.rb
+++ lib/rails/options_config/application.rb
@@ -1,7 +1,7 @@
 module Rails
   class Application
     def options env=nil
-      if env.nil?
+      if env.nil? || env.to_sym == Rails.env.to_sym
         @options ||= OptionsConfig.parse_options self, Rails.env.to_sym
       else
         OptionsConfig.parse_options self, env.to_sym
